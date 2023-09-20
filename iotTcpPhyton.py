94d50b7a71ac0b520eb892fcfef6aaefaa8d8228
vnunciatelli <145514131+vnunciatelli@users.noreply.github.com>
2023-09-19 21:48:00 -0300

    Create iotTcpPhyton.py

---
+++ iotTcpPhyton.py
@@ -0,0 +1,22 @@
+import socket
+
+# Configuração do servidor
+host = 'localhost'
+port = 12345
+
+# Inicialização do servidor TCP
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host, port))
+server_socket.listen(5)
+
+print(f"Aguardando conexão em {host}:{port}")
+client_socket, addr = server_socket.accept()
+print(f"Conexão estabelecida com {addr}")
+
+# Recebendo dados do dispositivo IoT
+data = client_socket.recv(1024)
+print(f"Dados recebidos: {data.decode()}")
+
+# Fechando a conexão
+client_socket.close()
+server_socket.close()
